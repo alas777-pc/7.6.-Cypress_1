ffc12f3f9dbebdb4704588b8ab8ae7c1b1d82b59
Alexei Lubinets <lubinets01@mail.ru>
2025-02-26 22:51:50 +0500

    Внесены корректировки

--- cypress/e2e/Book.cy.js
+++ cypress/e2e/Book.cy.js
@@ -30,3 +30,57 @@ describe('login spec', () => {
     });
     
   })
+
+
+
+  const bookFirst = {
+    title: "Бесконечная шутка",
+    description:
+      "Отправляйтесь в захватывающее интеллектуальное путешествие в поисках «Бесконечной шутки» – фильма, который способен убить любого, кто его посмотрит",
+    author: "Уоллес Д.",
+  };
+  
+  const bookSecond = {
+    title: "Слепой убийца",
+    description:
+      "Две сестры Айрис и Лора привыкли жить под покровительством состоятельного отца.",
+    author: "Этвуд М.",
+  };
+  
+  const bookThird = {
+    title: "Т",
+    description:
+      "Главный герой книги – граф Т., знаток боевых искусств. Он сбегает от царской охраны и отправляется в долгий путь, чтобы достичь Оптиной Пустыни.",
+    author: "Виктор Пелевин",
+  };
+  
+  describe("Favorite book spec", () => {
+    beforeEach(() => {
+      cy.visit("/");
+      cy.login("test@test.com", "test");
+    });
+  
+   
+  
+    it("Should add new book", () => {
+      cy.addBook(bookFirst);
+      cy.get(".card-title").should("contain.text", bookFirst.title);
+    });
+  
+    
+    it("Should add new book to favorite", () => {
+      cy.addFavoriteBook(bookSecond);
+      cy.visit("/favorites");
+      cy.get(".card-title").should("contain.text", bookSecond.title);
+    });
+  
+  
+  
+    it("Should delete book from favorite", () => {
+      cy.visit("/favorites");
+      cy.contains(bookSecond.title)
+        .should("be.visible")
+        .within(() => cy.get(".card-footer > .btn").click({ force: true }));
+      cy.contains(bookSecond.title).should("not.exist");
+    });
+  });
